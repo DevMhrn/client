667e22b4f1428a4ace42c9d770b088dc996a1b39
DevMhrn <debashismaharana7854@gmail.com>
2024-08-24 10:39:51 +0530

    "Made Added the frontend "

---
+++ src/pages/Login.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Button,Form, Input, message   } from "antd";
+import './registerLoginPage.css';
+
+function Login(){
+    return(
+        <div className="login-main">
+            <header className="App-header">
+                <main className="main-area mw-500 text-center px-3 ">
+                    <section className="left-section">
+                        <h1>
+                            Login to Quiz App
+                        </h1>
+                    </section>
+                    <section className="right-section">
+                    <Form layout="vertical" >  {/* //need to add onFinish function */}
+                        <Form.Item
+                            label="Email"
+                            htmlFor="email"
+                            name="email"
+                            className="d-block"
+                            rules={[{ required: true, message: "Email is required" }]}
+                            >
+                            <Input
+                                id="email"
+                                type="text"
+                                placeholder="Enter your Email"
+                            ></Input>
+                            </Form.Item>
+
+                            <Form.Item
+                            label="Password"
+                            htmlFor="password"
+                            name="password"
+                            className="d-block"
+                            rules={[{ required: true, message: "Password is required" }]}
+                            >
+                            <Input
+                                id="password"
+                                type="password"
+                                placeholder="Enter your Password"
+                                
+                            ></Input>
+                            </Form.Item>
+
+                            <Form.Item className="d-block">
+                            <Button
+                                type="primary"
+                                block
+                                htmlType="submit"
+                                style={{ fontSize: "1rem", fontWeight: "600", width:"80px" }}
+                            >
+                                Login
+                            </Button>
+                            </Form.Item>
+                        </Form>
+                        <section className='login-to-register'>
+                            <p>
+                                New User? <Link to="/register" className='register-now'>Register Here</Link>
+                            </p>
+                        </section>
+                    </section>
+
+                </main>
+
+            </header>
+        </div>
+    )
+}
+export default Login;
---
+++ src/pages/Register.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Form, Input, Button, Radio, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import './RegisterLogin.css';
+
+function Register() {
+    return (
+        <div className="Register-main">
+            <header className="App-header">
+                
+                <main className="main-area mw-500 text-center px-3">
+                <section className="left-section">
+                    <h1>Register to Quiz</h1>
+                </section>
+                <section className="right-section">
+                    <Form layout="vertical" >{/* //need to add onFinish function */}
+                    <Form.Item
+                        label="Name"
+                        htmlFor="name"
+                        name="name"
+                        className="d-block"
+                        rules={[{ required: true, message: "Name is required!" }]}
+                    >
+                        <Input
+                        id="name"
+                        type="text"
+                        placeholder="Enter your name"
+                        rules={[{ required: true, message: "Email is required!" }]}
+                        ></Input>
+                    </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        htmlFor="email"
+                        name="email"
+                        className="d-block"
+                        rules={[{ required: true, message: "Email is required!" }]}
+                    >
+                        <Input
+                        id="email"
+                        type="email"
+                        placeholder="Enter your email"
+                        ></Input>
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        htmlFor="password"
+                        name="password"
+                        className="d-block"
+                        rules={[{ required: true, message: "Password is required!" }]}
+                    >
+                        <Input
+                        id="password"
+                        type="password"
+                        placeholder="Enter the password"
+                        ></Input>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button
+                        block
+                        type="primary"
+                        htmlType="submit"
+                        style={{ fontSize: "1rem", fontWeight: "600", width:"80px" }}
+                        >
+                        Sign Up
+                        </Button>
+                    </Form.Item>
+                    <Form.Item
+                        label="Register as a Admin"
+                        htmlFor="role"
+                        name="role"
+                        className="d-block text-center"
+                        initialValue={false}
+                        rules={[{ required: true, message: "Please select an option!" }]}
+                    >
+                        <div className="d-flex justify-content-start">
+                        <Radio.Group
+                            name="radiogroup"
+                            className="flex-start"
+                        >
+                            <Radio value={'Admin'}>Yes</Radio>
+                            <Radio value={'User'}>No</Radio>
+                        </Radio.Group>
+                        </div>
+                    </Form.Item>
+                    </Form>
+                    <section className="register-to-login">
+                    <p>
+                        Already a user? <Link to="/login" className="login-now">Login now</Link>
+                    </p>
+                    </section>
+                </section>
+                </main>
+            </header>
+        </div>
+    )
+}
+export default Register;
